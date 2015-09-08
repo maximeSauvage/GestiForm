28f8296f97adcb4a7a5b736f8caa9e6f8935ddd7
maxime sauvage <msauvage@localhost.localdomain>
2015-09-09 01:19:50 +0200

    config to show jsf

--- gestiform/src/main/java/be/atc/gestiform/GestiformApplication.java
+++ gestiform/src/main/java/be/atc/gestiform/GestiformApplication.java
@@ -1,56 +1,50 @@
 package be.atc.gestiform;
 
 import javax.faces.webapp.FacesServlet;
-import javax.servlet.ServletContext;
+import javax.servlet.DispatcherType;
 
+import org.primefaces.webapp.filter.FileUploadFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
-import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.ServletContextAware;
-
-import com.sun.faces.config.ConfigureListener;
 
 @Configuration
-@EnableAutoConfiguration(exclude={WebMvcAutoConfiguration.class,DispatcherServletAutoConfiguration.class})
+@EnableAutoConfiguration
 @ComponentScan
-public class GestiformApplication implements ServletContextAware {
+public class GestiformApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestiformApplication.class, args);
-	}
 
-	/*
-	 * Below sets up the Faces Servlet for Spring Boot
-	 */
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(GestiformApplication.class);
+        app.run(args);
+    }
 
     @Bean
-    public ServletRegistrationBean facesServletRegistration() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(
-            new FacesServlet(), "*.xhtml");
+    public ServletRegistrationBean facesServletRegistraiton() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new FacesServlet(), new String[]{"*.xhtml"});
+        registration.setName("Faces Servlet");
         registration.setLoadOnStartup(1);
         return registration;
     }
 
     @Bean
-    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
-        return new ServletListenerRegistrationBean<ConfigureListener>(
-            new ConfigureListener());
+    public ServletContextInitializer servletContextInitializer() {
+        return servletContext -> {
+            servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+            servletContext.setInitParameter("primefaces.THEME", "bootstrap");
+            servletContext.setInitParameter("primefaces.CLIENT_SIDE_VALIDATION", Boolean.TRUE.toString());
+            servletContext.setInitParameter("javax.faces.FACELETS_SKIP_COMMENTS", Boolean.TRUE.toString());
+            servletContext.setInitParameter("primefaces.FONT_AWESOME", Boolean.TRUE.toString());
+        };
     }
+    
+    
 
-	// To get JSF working on Spring Boot without a web.xml or faces-config.xml
-	// you need to force it to load its configuration files via an init
-	// parameter on the ServletContext.
-	// An easy way to do that is to implement ServletContextAware
-	// @see
-	// http://stackoverflow.com/questions/25479986/spring-boot-with-jsf-could-not-find-backup-for-factory-javax-faces-context-face
-    @Override
-    public void setServletContext(ServletContext servletContext) {
-        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());       
-    }
 }
