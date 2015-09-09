522b55723ae8baa17729e2debdd4a1c48a5555b4
maxime sauvage <msauvage@localhost.localdomain>
2015-09-09 23:52:43 +0200

    create list view of Apprenant

---
+++ gestiform/src/main/java/be/atc/gestiform/apprenant/view/ApprenantListView.java
@@ -0,0 +1,21 @@
+package be.atc.gestiform.apprenant.view;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import be.atc.gestiform.apprenant.entity.Apprenant;
+import be.atc.gestiform.apprenant.service.ApprenantService;
+
+@Component("apprenantListView")
+@Scope("request")
+public class ApprenantListView {
+	
+	@Autowired
+	ApprenantService apprenantService;
+	
+	public Iterable<Apprenant> getAllApprenant() {
+		return apprenantService.findAllApprenant();
+	}
+
+}
