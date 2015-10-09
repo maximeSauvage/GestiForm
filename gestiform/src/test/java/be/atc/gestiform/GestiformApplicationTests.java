78cece442861e40e77954d83e152470fc3733eec
maxime sauvage <msauvage@localhost.localdomain>
2015-10-09 23:49:43 +0200

    test

--- gestiform/src/test/java/be/atc/gestiform/GestiformApplicationTests.java
+++ gestiform/src/test/java/be/atc/gestiform/GestiformApplicationTests.java
@@ -1,10 +1,16 @@
 package be.atc.gestiform;
 
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import be.atc.gestiform.util.WeekendQuery;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = GestiformApplication.class)
@@ -13,6 +19,15 @@ public class GestiformApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void check_if_date_is_weekend () {
+	    LocalDate date = LocalDate.of(2015, 01, 03); // samedi
+	    
+	    Boolean workDay = date.query(new WeekendQuery());
+
+	    assertTrue(workDay);
 	}
 
 }
